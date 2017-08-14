1dc9d7b27757c638f5b0adaea5dde91df8d724df
Sarulian <tedjamesrobinson@gmail.com>
2017-08-13 20:12:09 -0700

    string formatting

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
 
 while 1:
-	print("The measured voltage is: %s", % str(GPIO.input(16)))
+	print("The measured voltage is: {0}".format(GPIO.input(16)))
 	sleep(0.5)
